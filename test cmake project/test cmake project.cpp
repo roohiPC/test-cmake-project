7b5e58b2ed0691eb69c367e501b19667c5302167
roohi <roohi_iq@yahoo.com>
2018-06-04 19:57:10 +0430

    Add project files.

---
+++ test cmake project/test cmake project.cpp
@@ -0,0 +1,12 @@
+﻿// test cmake project.cpp : Defines the entry point for the application.
+//
+
+#include "test cmake project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
